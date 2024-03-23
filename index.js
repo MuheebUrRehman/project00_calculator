99a41ae6b9c815ac38b9418484557e308318cf38
MuheebUrRehman <muhiburrehman94@gmail.com>
2024-03-23 21:07:31 +0500

    push my calculator

---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const anwser = await inquirer.prompt([
+    {
+        message: "welcome to Simple Command Line Calculator(press ENTER KEY to continue)",
+        type: "text",
+        name: "msg",
+    },
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select any one operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: [
+            "addition",
+            "subtraction",
+            "multiplication",
+            "division",
+            "exponent",
+        ],
+    },
+]);
+if (anwser.operator === "addition") {
+    console.log(`your anwser is = ${anwser.firstnumber + anwser.secondnumber}`);
+}
+else if (anwser.operator === "subtraction") {
+    console.log(`your anwser is = ${anwser.firstnumber - anwser.secondnumber}`);
+}
+else if (anwser.operator === "multiplication") {
+    console.log(`your anwser is = ${anwser.firstnumber * anwser.secondnumber}`);
+}
+else if (anwser.operator === "division") {
+    console.log(`your anwser is = ${anwser.firstnumber / anwser.secondnumber}`);
+}
+else if (anwser.operator === "exponent") {
+    console.log(`your anwser is =${anwser.firstnumber ** anwser.secondnumber}`);
+}
+else {
+    console.log("please enter valid operator");
+}
